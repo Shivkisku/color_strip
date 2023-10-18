bb30c135d7dcdbe33579ff26dc4311ddca3fda9e
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2023-10-18 16:23:00 +0530

    color

---
+++ colorstrip.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+/*
+ *
+ * It's a rainbow rainbow.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+    char red[] = "\e[48;5;1m";
+    char orange[] = "\e[48;5;208m";
+    char yellow[] = "\e[48;5;226m";
+    char green[] = "\e[48;5;10m";
+    char blue[] = "\e[48;5;27m";
+    char indigo[] = "\e[48;5;57m";
+    char violet[] = "\e[48;5;54m";
+    char reset_the_rainbow_color[] = "\e[49m";
+
+    char *all_the_rainbow_colors[] = {red, orange, yellow, green, blue, indigo, violet};
+    int number_of_all_the_rainbow_colors = 6;
+    int the_current_rainbow_color = 0;
+
+    struct winsize w;
+
+    while(1)
+    {
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+        int rainbow_height = (int)(w.ws_col / 30);
+        int rainbow_width = w.ws_col;
+        for(int row = 0; row < rainbow_height; row++)
+        {
+            for(int col = 0; col < rainbow_width; col++)
+            {
+                printf("%s%c%s", all_the_rainbow_colors[the_current_rainbow_color], ' ', reset_the_rainbow_color);
+            }
+            printf("\n");
+        }
+
+        if(the_current_rainbow_color < number_of_all_the_rainbow_colors)
+            the_current_rainbow_color++;
+        else
+            the_current_rainbow_color = 0;
+        sleep(0.3);
+    }
+    return 0;
+}
